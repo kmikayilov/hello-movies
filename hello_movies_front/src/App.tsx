13a8d69a2e6a99e5c8370f80f8ee7348749c7ad6
Kanan Mikayilov <kenan.mikayilov.00@gmail.com>
2023-10-13 22:43:40 +0400

    loading added

--- hello_movies_front/src/App.tsx
+++ hello_movies_front/src/App.tsx
@@ -1,4 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useSelector, shallowEqual } from "react-redux"
+
+import LoadingIcon from "./assets/loading animated svg/loadin.svg"
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -9,19 +12,27 @@ import './App.css';
 import "./assets/css/custom.scss"
 
 function App() {
+
+  const isFetching = useSelector((state: any) => state.movie.isFetching, shallowEqual)
+
   return (
-    <div className="App">
-      <Header />
-      <div className="content-wrapper">
-        <Router>
-          <Routes>
-            <Route path="/" Component={List} />
-            <Route path="/detail/:id" Component={MovieDetails} />
-          </Routes>
-        </Router>
+    <>
+      <div className="App">
+        <Header />
+        <div className="content-wrapper">
+          <Router>
+            <Routes>
+              <Route path="/" Component={List} />
+              <Route path="/detail/:id" Component={MovieDetails} />
+            </Routes>
+          </Router>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+      {
+        isFetching && (<div className="loading"><img src={LoadingIcon} alt="loading_icon" /></div>)
+      }
+    </>
   );
 }
 
